17d1bacbd1dbba612ca0b2cde61113f2c64bbc4a
mmcl-dev <centenomagdalena@hotmail.com>
2020-11-16 22:58:09 -0300

    Ultimo merge

--- miFile1.js
+++ miFile1.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-Algo 1
-Agrego más lineas - Magda
+//Algo 1
+//Agrego más lineas - Magda
 /// hweuhdhwidwijcisjc
 console.log(1)
-=======
 //Algo 1
 let prueba = {
     "titulo": "Practicar el switch",
@@ -12,4 +10,3 @@ let prueba = {
    }
 //Algo 1
 //Agrego más lineas - Magda
->>>>>>> f9a56932e8520337156abd2d0972ebb513fd4592
